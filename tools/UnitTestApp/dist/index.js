5503b0686733b1da6325bff5d4b833d17376ee2e
srmukher <120183316+srmukher@users.noreply.github.com>
2023-11-24 11:03:44 +0530

    Update index.js

--- tools/UnitTestApp/dist/index.js
+++ tools/UnitTestApp/dist/index.js
@@ -30357,19 +30357,31 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(4769);
 const github = __nccwpck_require__(1433);
+const fs = __nccwpck_require__(7147);
+const readFilePath = 'tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/components/Sparkline/Sparkline.base.js';
+const writeFilePath = 'tools/UnitTestApp/Sparkline/SparklineBase.js';
+// Read the file
+const updatedFile = () => {
+  fs.readFile(readFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file from disk: ${err}`);
+    } else {
+      // Replace the words
+      let result = data.replace(/require\("d3-scale"\)/g, "import('d3-scale')");
+      result = result.replace(/require\("d3-shape"\)/g, "import('d3-shape')");
+      result = result.replace(/require\("d3-array"\)/g, "import('d3-array')");
+      // Write the file back
+      fs.writeFile(writeFilePath, result, 'utf8', err => {
+        if (err) {
+          console.error(`Error writing file to disk: ${err}`);
+        }
+      });
+    }
+  });
+};
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+updatedFile();
+
 })();
 
 module.exports = __webpack_exports__;
