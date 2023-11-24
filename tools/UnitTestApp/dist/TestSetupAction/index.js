f3e12bbe822f7a5555f694bc27125b7bd6be24e3
srmukher <120183316+srmukher@users.noreply.github.com>
2023-11-24 13:21:19 +0530

    Update index.js

--- tools/UnitTestApp/dist/TestSetupAction/index.js
+++ tools/UnitTestApp/dist/TestSetupAction/index.js
@@ -1,6 +1,110 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 2070:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(4769);
+const github = __nccwpck_require__(1433);
+const fs = __nccwpck_require__(7147);
+const path = __nccwpck_require__(1017);
+
+async function readDirectoryDeep(dirPath) {
+  const items = await fs.readdirSync(dirPath);
+
+  items.forEach(item => {
+      const fullPath = path.join(dirPath, item);
+      const stat = fs.statSync(fullPath);
+
+      if (stat.isDirectory()) {
+          readDirectoryDeep(fullPath);
+      } else {
+          updatedFileDeep(fullPath);
+      }
+  });
+}
+
+// Read the file
+async function updatedFileDeep(filePath) {
+  let data = await fs.readFileSync(filePath, 'utf8'); 
+  let matches = data.match(/.*=.*d3.*(\n.*?)*(?=var|$)/g);
+  if (matches != null && matches.length > 0) {
+    matches.forEach(match => {
+      if (match !=null) {
+        data = data.replace(match, '');
+      }
+    });
+    // Write the file back
+    if (data != null && data != '' && data != undefined) {
+      await fs.writeFileSync(filePath, data);
+    }
+  }
+};
+
+async function readDirectory(dirPath) {
+  const items = await fs.readdirSync(dirPath);
+
+  items.forEach(item => {
+      const fullPath = path.join(dirPath, item);
+      const stat = fs.statSync(fullPath);
+
+      if (stat.isDirectory()) {
+          readDirectory(fullPath);
+      } else {
+          updatedFile(fullPath);
+      }
+  });
+}
+
+async function updatedFile(filePath) {
+  let data = await fs.readFileSync(filePath, 'utf8'); 
+  // Replace the words
+  data = data.replace(/require\("d3-scale"\)/g, "import('d3-scale')");
+  data = data.replace(/require\("d3-shape"\)/g, "import('d3-shape')");
+  data = data.replace(/require\("d3-array"\)/g, "import('d3-array')");
+  data = data.replace(/require\("d3-selection"\)/g, "import('d3-selection')");
+  data = data.replace(/require\("d3-format"\)/g, "import('d3-format')");
+  data = data.replace(/require\("d3-axis"\)/g, "import('d3-axis')");
+  data = data.replace(/require\("d3-time"\)/g, "import('d3-time')");
+  data = data.replace(/require\("d3-time-format"\)/g, "import('d3-time-format')");
+  // Write the file back
+  if (data != null && data != '' && data != undefined) {
+    await fs.writeFileSync(filePath, data);
+  }
+};
+
+function setupIndexFile(indexFilePath, fileName) {
+    fs.readFile(indexFilePath, 'utf8', (err, data) => {
+        if (!err) {
+            // Replace the words
+            let result = data.replace(/.*exportStar.*/g, '');            
+            // Write the file back
+            fs.writeFile(indexFilePath, result, 'utf8', err => {
+                if (err) {
+                    core.setOutput("Error",  `Error writing file to disk: ${err}`);
+                }
+            });
+        }
+    });
+}
+
+const testSetupRewire = () => {
+  readDirectory('tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/');
+  readDirectoryDeep('tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/components/');
+  const chart = __nccwpck_require__(8938);
+  core.setOutput("ModifiedChart", chart.toString());
+  const indexFilePath = 'tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/index.js';
+  setupIndexFile(indexFilePath, null);
+  const indexFileContents = __nccwpck_require__(718);
+  core.setOutput("ModifiedIndex", indexFileContents.toString());
+};
+
+testSetupRewire();
+module.exports=testSetupRewire;
+
+
+/***/ }),
+
 /***/ 4507:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -28471,6 +28575,22 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 8938:
+/***/ ((module) => {
+
+module.exports = eval("require")("tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/components/AreaChart/AreaChart.base.js");
+
+
+/***/ }),
+
+/***/ 718:
+/***/ ((module) => {
+
+module.exports = eval("require")("tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/index.js");
+
+
+/***/ }),
+
 /***/ 9491:
 /***/ ((module) => {
 
@@ -30352,38 +30472,12 @@ module.exports = parseParams
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(4769);
-const github = __nccwpck_require__(1433);
-const fs = __nccwpck_require__(7147);
-const readFilePath = 'tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/components/Sparkline/Sparkline.base.js';
-const writeFilePath = 'tools/UnitTestApp/Sparkline/SparklineBase.js';
-// Read the file
-const updatedFile = () => {
-  fs.readFile(readFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading file from disk: ${err}`);
-    } else {
-      // Replace the words
-      let result = data.replace(/require\("d3-scale"\)/g, "import('d3-scale')");
-      result = result.replace(/require\("d3-shape"\)/g, "import('d3-shape')");
-      result = result.replace(/require\("d3-array"\)/g, "import('d3-array')");
-      // Write the file back
-      fs.writeFile(writeFilePath, result, 'utf8', err => {
-        if (err) {
-          console.error(`Error writing file to disk: ${err}`);
-        }
-      });
-    }
-  });
-};
-
-updatedFile();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(2070);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
