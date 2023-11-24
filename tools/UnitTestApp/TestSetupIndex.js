9fe666eea9f5bd441a844407314ca33f1c62d4de
srmukher <120183316+srmukher@users.noreply.github.com>
2023-11-24 11:29:00 +0530

    Update TestSetupIndex.js

--- tools/UnitTestApp/TestSetupIndex.js
+++ tools/UnitTestApp/TestSetupIndex.js
@@ -1,26 +1,3 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
-const readFilePath = 'tools/UnitTestApp/node_modules/@fluentui/react-charting/lib-commonjs/components/Sparkline/Sparkline.base.js';
-const writeFilePath = 'tools/UnitTestApp/Sparkline/SparklineBase.js';
-// Read the file
-const updatedFile = () => {
-  fs.readFile(readFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading file from disk: ${err}`);
-    } else {
-      // Replace the words
-      let result = data.replace(/require\("d3-scale"\)/g, "import('d3-scale')");
-      result = result.replace(/require\("d3-shape"\)/g, "import('d3-shape')");
-      result = result.replace(/require\("d3-array"\)/g, "import('d3-array')");
-      // Write the file back
-      fs.writeFile(writeFilePath, result, 'utf8', err => {
-        if (err) {
-          console.error(`Error writing file to disk: ${err}`);
-        }
-      });
-    }
-  });
-};
-
-updatedFile();
+require('tools/UnitTestApp/script');
