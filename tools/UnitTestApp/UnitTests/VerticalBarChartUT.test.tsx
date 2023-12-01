16118b7636a6d9c5c6ea70be4b1d56a8916cc6fc
srmukher <120183316+srmukher@users.noreply.github.com>
2023-12-01 17:57:09 +0530

    Update VerticalBarChartUT.test.tsx

--- tools/UnitTestApp/UnitTests/VerticalBarChartUT.test.tsx
+++ tools/UnitTestApp/UnitTests/VerticalBarChartUT.test.tsx
@@ -1,12 +1,12 @@
 const basePath = fs.realpathSync('./') + '/src/components/VerticalBarChart';
-import { stringPoints } from basePath + '/VerticalBarChartRTL.test';
+import { stringPoints } from '../VerticalBarChart/VerticalBarChartRTL.test';
 import { DarkTheme } from '@fluentui/theme-samples';
-import { chartPoints } from basePath + '/VerticalBarChart.test';
-import { testWithoutWait } from fs.realpathSync('./') + '/src/utilities/TestUtility.test';
+import { chartPoints } from '../VerticalBarChart/VerticalBarChart.test';
+import { testWithoutWait } from '../../utilities/TestUtility.test';
 import * as utils from '@fluentui/react/lib/Utilities';
 import { DefaultPalette } from '@fluentui/react';
-import { VerticalBarChartBase } from basePath + '/VerticalBarChart.base';
-import { VerticalBarChart } from basePath + '/VerticalBarChart';
+import { VerticalBarChartBase } from '../VerticalBarChart/VerticalBarChart.base';
+import { VerticalBarChart } from '../VerticalBarChart/VerticalBarChart';
 
 import * as fs from 'fs';
 
