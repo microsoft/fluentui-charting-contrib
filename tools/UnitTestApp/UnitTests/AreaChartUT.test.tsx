2cd1ff78d87d29ab707dc3391ef254246e4cd6ec
srmukher <120183316+srmukher@users.noreply.github.com>
2023-12-05 12:31:01 +0530

    Update AreaChartUT.test.tsx

--- tools/UnitTestApp/UnitTests/AreaChartUT.test.tsx
+++ tools/UnitTestApp/UnitTests/AreaChartUT.test.tsx
@@ -5,18 +5,6 @@ import { getById, testWithoutWait } from '../../utilities/TestUtility.test';
 import { DarkTheme } from '@fluentui/theme-samples';
 import * as fs from 'fs';
 
-async function updatedFile(filePath: string) {
-  let data = await fs.readFileSync(filePath, 'utf8');
-  // Replace the words
-  data = data.replace(/\bprivate\b/g, "public");
-  // Write the file back
-  if (data != null && data != '' && data != undefined) {
-    await fs.writeFileSync(filePath, data);
-  }
-};
-
-updatedFile(fs.realpathSync('./') + '/src/components/AreaChart/AreaChart.base.tsx');
-
 const chart1Points = [
   {
     x: 20,
