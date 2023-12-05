57ca6796d52cf49fb4f927ea9f84ec7e9b9af7d1
srmukher <120183316+srmukher@users.noreply.github.com>
2023-12-05 10:48:14 +0530

    Create TestEnvSetupScript.ts

---
+++ tools/UnitTestApp/TestEnvSetupScript.ts
@@ -0,0 +1,9 @@
+async function updatedFile(filePath: string) {
+  let data = await fs.readFileSync(filePath, 'utf8');
+  // Replace the words
+  data = data.replace(/\bprivate\b/g, "public");
+  // Write the file back
+  if (data != null && data != '' && data != undefined) {
+    await fs.writeFileSync(filePath, data);
+  }
+};
