a5dada9916523675cc6364df8ff97a97f8e577c4
srmukher <120183316+srmukher@users.noreply.github.com>
2023-12-05 12:31:21 +0530

    Update VerticalBarChartUT.test.tsx

--- tools/UnitTestApp/UnitTests/VerticalBarChartUT.test.tsx
+++ tools/UnitTestApp/UnitTests/VerticalBarChartUT.test.tsx
@@ -11,18 +11,6 @@ import * as fs from 'fs';
 
 import { IVerticalBarChartDataPoint } from '../../index';
 
-async function updatedFile(filePath: string) {
-  let data = await fs.readFileSync(filePath, 'utf8');
-  // Replace the words
-  data = data.replace(/\bprivate\b/g, "public");
-  // Write the file back
-  if (data != null && data != '' && data != undefined) {
-    await fs.writeFileSync(filePath, data);
-  }
-};
-
-updatedFile(fs.realpathSync('./') + '/src/components/VerticalBarChart/VerticalBarChart.base.tsx');
-
 const stringPoints = [
   {
     x: 'medium',
