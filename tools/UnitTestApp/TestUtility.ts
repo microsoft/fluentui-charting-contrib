3c42278091b2de194b01df3a354b70b87c7d6808
srmukher <srmukher@microsoft.com>
2024-01-05 19:24:44 +0530

    Cleaning up UnitTestApp module

--- tools/UnitTestApp/TestUtility.ts
+++
@@ -1,8 +0,0 @@
-function testWithRewire(pathToModule, baseClass, testFunction) {
-    const rewire = require('rewire');
-    const module = rewire(pathToModule);
-    const chartBase = module.__get__(baseClass);
-    testFunction(chartBase);
-}
-
-module.exports = testWithRewire;
