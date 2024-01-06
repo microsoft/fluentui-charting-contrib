453266f68bbc33c4c6cb7209bd4217397398e4bd
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-01-06 20:39:15 +0530

    Add url config

--- apps/fluentui-react-charting-docusaurus/docusaurus.config.ts
+++ apps/fluentui-react-charting-docusaurus/docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'fluentui-charting-contrib-docsite',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://microsoft.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/fluentui-charting-contrib/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'microsoft', // Usually your GitHub org/user name.
+  projectName: 'fluentui-charting-config', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
