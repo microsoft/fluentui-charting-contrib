4642ae7c1aa3c6236470549167c216339e5aa163
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-01-07 12:42:50 +0530

    Title change

--- apps/fluentui-react-charting-docusaurus/docusaurus.config.ts
+++ apps/fluentui-react-charting-docusaurus/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "fluentui-charting-contrib-docsite",
+  title: "FluentUI Charting Contrib Docsite",
   tagline: "FluentUI docsite",
   favicon: "img/favicon.ico",
 
