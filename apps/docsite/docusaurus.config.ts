797e284dbf209aae0c07b0ef87c9a4d75c1bbaea
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-01-07 14:57:44 +0530

    tagline change

--- apps/docsite/docusaurus.config.ts
+++ apps/docsite/docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "FluentUI Charting Contrib Docsite",
-  tagline: "MD docs related to Charting library",
+  tagline: "MD docs related to FluentUI Charting library",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
