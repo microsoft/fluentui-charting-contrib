abc4b3c141df82e132ecbac57778a438e07fd53a
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-01-09 18:33:30 +0530

    checkout main while deploying

--- apps/docsite/docusaurus.config.ts
+++ apps/docsite/docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://microsoft.github.io",
+  url: "https://shubhabrata08.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/fluentui-charting-contrib/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "microsoft", // Usually your GitHub org/user name.
+  organizationName: "Shubhabrata08", // Usually your GitHub org/user name.
   projectName: "fluentui-charting-contrib", // Usually your repo name.
   deploymentBranch: "main",
   trailingSlash: false,
