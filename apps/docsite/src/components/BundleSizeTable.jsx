fdd18d9cb5a522fd6acecf48ee5bb8eb280630c8
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-02-10 14:05:54 +0530

    Add Table for bundleSize

--- apps/docsite/src/components/BundleSizeTable.jsx
+++ apps/docsite/src/components/BundleSizeTable.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 const BundleSizeTable = () => {
     const [bundleSizeData, setBundleSizeData] = useState();
-    const createTableFromJSON = (JSONData) =>{
-        
+    const [bundleSizeTable, setBundleSizeTable] = useState();
+    const createTableFromJSON = (JSONData) => {
+        JSONData.map((item, index) => {
+            console.log(item)
+        })
     }
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("https://raw.githubusercontent.com/microsoft/fluentui-charting-contrib/test-coverage-artifacts/windowsCoverage.json");
+                const response = await fetch("https://raw.githubusercontent.com/Shubhabrata08/fluentui/add-monosize-measure-json/packages/react-charting/monosize.json");
                 if (!response) {
                     throw new Error('Invalid response');
                 }
@@ -20,9 +23,36 @@ const BundleSizeTable = () => {
         }
         fetchData();
     }, []);
+    useEffect(() => {
+        if (bundleSizeData) {
+            createTableFromJSON(bundleSizeData)
+        }
+    }, [bundleSizeData])
     return (
         <>
-            {JSON.stringify(bundleSizeData)}
+            {bundleSizeData? (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>MIN size</th>
+                            <th>GZIP size</th>
+                            {/* Add more headings if needed */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bundleSizeData.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td> 
+                                <td>{((item.minifiedSize)/1024).toFixed(2)} KiB</td> 
+                                <td>{((item.gzippedSize)/1024).toFixed(2)} KiB</td> 
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p>Loading...</p>
+            )}
         </>
     )
 }
