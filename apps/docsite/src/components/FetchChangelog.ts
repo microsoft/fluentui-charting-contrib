4d11938cfcaaa8c59a01f623ed3d1fd7ef231cfb
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-02-18 15:08:51 +0530

    Fix arrangement of change type

--- apps/docsite/src/components/FetchChangelog.ts
+++ apps/docsite/src/components/FetchChangelog.ts
@@ -9,34 +9,36 @@ const fetchChangelog = async() => {
       let versionSplit = item.version.split('.')
       console.log(versionSplit)
       const minorVersion=(versionSplit[0]+"."+versionSplit[1]);
-      if("patch" in item.comments){
-        item.comments.patch.map((patchItem,patchIndex)=>{
-          fs.appendFile(minorVersion+".md",patchItem.comment+"\n",(err)=>{
-            if(err) throw err;
-          });
-        })
-      }
-      if("none" in item.comments){
-        item.comments.none.map((noneItem,noneIndex)=>{
-          fs.appendFile(minorVersion+".md",noneItem.comment+"\n",(err)=>{
+      if("major" in item.comments){
+        item.comments.major.map((majorItem,majorIndex)=>{
+          fs.appendFile(minorVersion+".md","- "+majorItem.comment+"\n",(err)=>{
             if(err) throw err;
           });
         })
       }
       if("minor" in item.comments){
         item.comments.minor.map((minorItem,minorIndex)=>{
-          fs.appendFile(minorVersion+".md",minorItem.comment+"\n",(err)=>{
+          fs.appendFile(minorVersion+".md","- "+minorItem.comment+"\n",(err)=>{
             if(err) throw err;
           });
         })
       }
-      if("major" in item.comments){
-        item.comments.major.map((majorItem,majorIndex)=>{
-          fs.appendFile(minorVersion+".md",majorItem.comment+"\n",(err)=>{
+      if("patch" in item.comments){
+        item.comments.patch.map((patchItem,patchIndex)=>{
+          fs.appendFile(minorVersion+".md","- "+patchItem.comment+"\n",(err)=>{
             if(err) throw err;
           });
         })
       }
+      if("none" in item.comments){
+        item.comments.none.map((noneItem,noneIndex)=>{
+          fs.appendFile(minorVersion+".md","- "+noneItem.comment+"\n",(err)=>{
+            if(err) throw err;
+          });
+        })
+      }
+      
+      
     }
       
     
