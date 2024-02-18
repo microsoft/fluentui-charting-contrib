47bd44c8a7ae8f619dad84fda8d218833f5a13e2
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-02-18 15:13:53 +0530

    Fix linting

--- apps/docsite/src/components/FetchChangelog.ts
+++ apps/docsite/src/components/FetchChangelog.ts
@@ -1,47 +1,54 @@
-const fs = require("fs")
-
-const fetchChangelog = async() => {
-  const data=await fetch("https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-charting/CHANGELOG.json").then(res=>res.json())
-  data.entries.map((item,index)=>{
-    let currVersion="";
-    if(item.tag.startsWith("@fluentui/react-charting"))
-    {
-      let versionSplit = item.version.split('.')
-      console.log(versionSplit)
-      const minorVersion=(versionSplit[0]+"."+versionSplit[1]);
-      if("major" in item.comments){
-        item.comments.major.map((majorItem,majorIndex)=>{
-          fs.appendFile(minorVersion+".md","- "+majorItem.comment+"\n",(err)=>{
-            if(err) throw err;
-          });
-        })
-      }
-      if("minor" in item.comments){
-        item.comments.minor.map((minorItem,minorIndex)=>{
-          fs.appendFile(minorVersion+".md","- "+minorItem.comment+"\n",(err)=>{
-            if(err) throw err;
-          });
-        })
-      }
-      if("patch" in item.comments){
-        item.comments.patch.map((patchItem,patchIndex)=>{
-          fs.appendFile(minorVersion+".md","- "+patchItem.comment+"\n",(err)=>{
-            if(err) throw err;
-          });
-        })
-      }
-      if("none" in item.comments){
-        item.comments.none.map((noneItem,noneIndex)=>{
-          fs.appendFile(minorVersion+".md","- "+noneItem.comment+"\n",(err)=>{
-            if(err) throw err;
-          });
-        })
-      }
-      
-      
-    }
-      
-    
-  })
+const fs = require("fs");
+function throwError(err) {
+  if (err) throw err;
 }
+const fetchChangelog = async () => {
+  const data = await fetch(
+    "https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-charting/CHANGELOG.json"
+  ).then((res) => res.json());
+  data.entries.map((item, index) => {
+    if (item.tag.startsWith("@fluentui/react-charting")) {
+      let versionSplit = item.version.split(".");
+      console.log(versionSplit);
+      const minorVersion = versionSplit[0] + "." + versionSplit[1];
+      if ("major" in item.comments) {
+        fs.appendFile(minorVersion + ".md", "### Major Changes\n", throwError);
+        item.comments.major.map((majorItem, majorIndex) => {
+          fs.appendFile(
+            minorVersion + ".md",
+            "- " + majorItem.comment + "\n",
+            throwError
+          );
+        });
+      }
+      if ("minor" in item.comments) {
+        fs.appendFile(minorVersion + ".md", "### Minor Changes\n", throwError);
+        item.comments.minor.map((minorItem, minorIndex) => {
+          fs.appendFile(
+            minorVersion + ".md",
+            "- " + minorItem.comment + "\n",
+            throwError
+          );
+        });
+      }
+      if ("patch" in item.comments) {
+        fs.appendFile(minorVersion + ".md", "### Patches\n", throwError);
+        item.comments.patch.map((patchItem, patchIndex) => {
+          fs.appendFile(
+            minorVersion + ".md",
+            "- " + patchItem.comment + "\n",
+            throwError
+          );
+        });
+      }
+      // if("none" in item.comments){
+      //   item.comments.none.map((noneItem,noneIndex)=>{
+      //     fs.appendFile(minorVersion+".md","- "+noneItem.comment+"\n",(err)=>{
+      //       if(err) throw err;
+      //     });
+      //   })
+      // }
+    }
+  });
+};
 fetchChangelog();
