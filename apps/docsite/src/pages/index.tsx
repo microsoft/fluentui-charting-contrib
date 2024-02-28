5aae02f291b389da72192dac5833a08a0cf4844b
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-02-28 20:17:34 +0530

    Fix CSS

--- apps/docsite/src/pages/index.tsx
+++ apps/docsite/src/pages/index.tsx
@@ -1,38 +1,27 @@
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
 import sidebars from "@site/sidebars";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+const HeaderBanner = ():JSX.Element => {
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div
-        className="container"
-        style={{ backgroundImage: "url(img/img_header.png)" }}
-      >
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to={`/docs/${sidebars.tutorialSidebar[0]}`}
-          >
-            Getting Started 🚈
-          </Link>
-        </div>
+    <>
+      <div className={`container ${styles.headerImageContainer}`}>
+        <img className={styles.headerImage} src={require("@site/static/img/img_header.png").default} />
       </div>
-    </header>
+      <div className={styles.buttons}>
+        <Link
+          className="button button--secondary button--lg"
+          to={`/docs/${sidebars.tutorialSidebar[0]}`}
+        >
+          Getting Started 🚈
+        </Link>
+      </div>
+    </>
   );
-}
-
+};
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -41,18 +30,7 @@ export default function Home(): JSX.Element {
         title={`${siteConfig.title}`}
         description="Description will go into a meta tag in <head />"
       >
-        <div className={`container ${styles.headerImage}`}>
-          <img src={require("@site/static/img/img_header.png").default} />
-        </div>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to={`/docs/${sidebars.tutorialSidebar[0]}`}
-          >
-            Getting Started 🚈
-          </Link>
-        </div>
-        {/* <HomepageHeader /> */}
+        <HeaderBanner/>
         <main>
           <HomepageFeatures />
         </main>
