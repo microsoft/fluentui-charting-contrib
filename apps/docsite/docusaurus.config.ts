b909bb16d388dc4c364385166a7e30fd180d8e71
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2024-02-28 20:26:42 +0530

    Minor fixes

--- apps/docsite/docusaurus.config.ts
+++ apps/docsite/docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/fluent-charting-social-card.jpg",
     navbar: {
       title: "FluentUI Charting",
       logo: {
