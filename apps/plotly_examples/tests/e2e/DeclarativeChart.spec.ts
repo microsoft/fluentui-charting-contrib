05ddd6b0f69f65dea2534195d136f66378853b99
v-baambati <132879294+v-baambati@users.noreply.github.com>
2024-12-27 13:38:46 +0530

    updated snapshots

--- apps/plotly_examples/tests/e2e/DeclarativeChart.spec.ts
+++ apps/plotly_examples/tests/e2e/DeclarativeChart.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has chart', async ({ page }) => {
+test('Declarative Chart', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await expect(page.getByText('Data Visualization')).toBeVisible();
