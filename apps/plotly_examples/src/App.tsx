5e7216e46aee82a99ac6e7dced8e3a9568e9ed6d
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2024-12-28 18:09:39 +0530

    Add portal compat provider

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -9,10 +9,11 @@ import {
   OptionOnSelectData,
   Subtitle2
 } from '@fluentui/react-components';
+import { PortalCompatProvider } from '@fluentui/react-portal-compat';
 import { ChartWrapper } from './components/ChartWrapper';
 
 const App: React.FC = () => {
-  const [value, setValue] = React.useState("Light");
+  const [value, setValue] = React.useState("Dark");
 
   const onOptionSelect = (event: SelectionEvents, data: OptionOnSelectData): void => {
     setValue(data.optionText ?? "");
@@ -20,16 +21,18 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <FluentProvider theme={ value === "Light"? webLightTheme: webDarkTheme} >
-        <Subtitle2> Theme:</Subtitle2>&nbsp;&nbsp;
-        <Dropdown
-            value={value}
-            onOptionSelect={onOptionSelect}
-          >
-            <Option>Light</Option>
-            <Option>Dark</Option>
-          </Dropdown>
-        <ChartWrapper />
+      <FluentProvider theme={ value === "Light"? webLightTheme: webDarkTheme} targetDocument={document}>
+        <PortalCompatProvider>
+          <Subtitle2> Theme:</Subtitle2>&nbsp;&nbsp;
+          <Dropdown
+              value={value}
+              onOptionSelect={onOptionSelect}
+            >
+              <Option>Light</Option>
+              <Option>Dark</Option>
+            </Dropdown>
+          <ChartWrapper />
+        </PortalCompatProvider>
       </FluentProvider>
     </div>
   );
