6238052b1368382f82ae9f3ea7950235859974d2
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-01-10 10:15:46 +0530

    added VR tests to plotly examples

--- apps/plotly_examples/tests/e2e/DeclarativeChart.spec.ts
+++ apps/plotly_examples/tests/e2e/DeclarativeChart.spec.ts
@@ -1,23 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('Declarative Chart', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://fluentchartstest.azurewebsites.net/');
 
-  await expect(page.getByText('Data Visualization')).toBeVisible();
   const combobox = page.getByRole('combobox');
-  await expect(combobox).toHaveCount(1);
-  await combobox.click();
-  const listbox = page.locator('.ms-Callout-main');
-  await expect(listbox).toHaveCount(1);
-  const listitems = listbox.getByRole('option');
+  await combobox.last().click();
+  const listbox = page.getByRole('listbox');
+  const listitems = listbox.last().getByRole('option');
   const lisItemsCount = await listitems.count();
+  const chartsListWithErrors = [33, 84, 87, 90, 91, 92, 98, 99, 100, 101, 167,168, 169, 170, 182, 183, 184, 185, 188, 189, 190, 191, 194, 195];
   for (let i = 0; i < lisItemsCount; i++) {
-    if (i != 33) {
+    if (!chartsListWithErrors.includes(i)) {
       await listitems.nth(i).scrollIntoViewIfNeeded();
       await listitems.nth(i).click();
       const chart = page.getByTestId('chart-container');
       await expect(chart).toHaveScreenshot();
-      await combobox.click();
+      await combobox.last().click();
     }
   }
 });
