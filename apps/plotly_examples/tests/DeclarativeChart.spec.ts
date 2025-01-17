db09c44b318b0a41efea1eba8fa739d6b860f8bb
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-01-17 14:10:58 +0530

    updated base url related chnages

--- apps/plotly_examples/tests/DeclarativeChart.spec.ts
+++ apps/plotly_examples/tests/DeclarativeChart.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect, Locator } from '@playwright/test';
+import * as dotenv from 'dotenv';
 
 const chartsListWithErrors = [33, 84, 87, 90, 91, 92, 98, 99, 100, 101, 167,168, 169, 170, 182, 183, 184, 185, 188, 189, 190, 191, 194, 195];
 var totalChartExamplesCount = 252;
+
 test.beforeEach( async ({ page }) => {
-  await page.goto('https://fluentchartstest-stage.azurewebsites.net/');
+  //Pass base URL as part of playwright command 
+  //ex:  npx cross-env BASE_URL='https://fluentchartstest-stage.azurewebsites.net/' npx playwright test
+  await page.goto(process.env.BASE_URL);
 });
 
 for (let index = 0; index < totalChartExamplesCount; index++) {
