355750fdccf269ef0f14507c224a8e41d739db54
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-01-18 09:22:08 +0530

    enable yarn link (#114)
    * Add more test schemas
    
    * Enable yarn link
    
    ---------
    
    Co-authored-by: Atishay Jain (from Dev Box) <atisjai@microsoft.com>

--- apps/plotly_examples/src/components/ChartWrapper.tsx
+++ apps/plotly_examples/src/components/ChartWrapper.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider, createTheme } from '@fluentui/react'
 import React from 'react';
 import { paletteSlots, semanticSlots } from "../theming/v8TokenMapping";
-import { DeclarativeChartBasicExample } from './DeclarativeChart';
+import DeclarativeChartBasicExample from './DeclarativeChart';
 
 export function ChartWrapper() {
     const v8Theme = createTheme({ palette: paletteSlots, semanticColors: semanticSlots }); //ToDo - Make the slot values dynamic
