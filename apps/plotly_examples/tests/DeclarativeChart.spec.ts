8defcd4dd45d53f54f6e4a8b6de3b5d5817a2aab
Anush Gupta <74965306+Anush2303@users.noreply.github.com>
2025-02-01 08:23:13 +0000

    amend

--- apps/plotly_examples/tests/DeclarativeChart.spec.ts
+++ apps/plotly_examples/tests/DeclarativeChart.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Locator } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 const chartsListWithErrors = [];
-var totalChartExamplesCount = 5;
+var totalChartExamplesCount = 302;
 
 test.beforeEach( async ({ page }) => {
   //Pass base URL as part of playwright command 
