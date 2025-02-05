c9a16744e23f8e0f3cf7943627910b65bb528136
Anush Gupta <74965306+Anush2303@users.noreply.github.com>
2025-02-05 03:43:40 +0000

    set trace off

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
   },
   timeout: 30000,
   /* Configure projects for major browsers */
