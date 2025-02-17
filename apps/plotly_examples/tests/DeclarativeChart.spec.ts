2347145d6cfdb57f0982b7516e4d9b1d23f3696f
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-02-17 08:32:08 +0530

    Update baseline screenshot (#130)
    Co-authored-by: Atishay Jain (from Dev Box) <atisjai@microsoft.com>

--- apps/plotly_examples/tests/DeclarativeChart.spec.ts
+++ apps/plotly_examples/tests/DeclarativeChart.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Locator } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 const chartsListWithErrors = [];
-var totalChartExamplesCount = 302;
+var totalChartExamplesCount = 332;
 const themes = ["Light", "Dark"];
 const modes = ["LTR", "RTL"];
 test.beforeEach(async ({ page }) => {
