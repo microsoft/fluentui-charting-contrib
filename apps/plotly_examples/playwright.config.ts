6c720173009c79fa1f9ba887f22ffba69f2441ca
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-05-24 07:38:09 +0530

    Fix plotly coverage report

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -20,9 +20,12 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // HTML report
+    ['json', { outputFile: 'playwright-report.json' }] // JSON report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
