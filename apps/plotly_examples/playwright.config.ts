51348b4dbcaef7ce7f0d566b8441ab6f78042865
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-05-24 12:50:04 +0530

    Merge upstream changes

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -22,13 +22,17 @@ export default defineConfig({
   /* Use 4 parallel workers on CI. Default otherwise */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // HTML report
+    ['json', { outputFile: 'playwright-report.json' }] // JSON report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    timezoneId: 'GMT',
     trace: 'off',
   },
   timeout: 30000,
