27ce043689a50985aa63cef48c4a96826065d8d0
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-05-24 12:43:38 +0530

    Fix bugs and update worker count

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -19,20 +19,16 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Enable parallel tests on CI and local */
-  workers: undefined,
+  /* Use 4 parallel workers on CI. Default otherwise */
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html', { outputFolder: 'playwright-report' }], // HTML report
-    ['json', { outputFile: 'playwright-report.json' }] // JSON report
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    timezoneId: 'GMT',
     trace: 'off',
   },
   timeout: 30000,
