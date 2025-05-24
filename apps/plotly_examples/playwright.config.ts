828a6ae8816f0859ebeb13a5d0d75a3165426eab
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-05-24 10:18:00 +0530

    Fix plotly example playwright workflow (#154)
    * Update charting to 5.25.89
    
    * Fix plotly coverage report
    
    * Fix issues in other actions
    
    * Use default number of workers
    
    ---------
    
    Co-authored-by: Atishay Jain (from Dev Box) <atisjai@microsoft.com>

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -19,10 +19,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Enable parallel tests on CI and local */
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // HTML report
+    ['json', { outputFile: 'playwright-report.json' }] // JSON report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
