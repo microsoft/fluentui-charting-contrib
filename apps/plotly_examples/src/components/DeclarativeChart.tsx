8fa8a765f9f19bfbdb8756433c136a87e8d8c013
Anush <anushgupta@microsoft.com>
2025-05-28 18:46:23 +0530

    Add examples for chart table

--- apps/plotly_examples/src/components/DeclarativeChart.tsx
+++ apps/plotly_examples/src/components/DeclarativeChart.tsx
@@ -50,10 +50,10 @@ type DataType =
 
 const dataTypeRanges = {
   'general': [{ min: 1, max: 252 }, {min: 750, max: 758 }],
-  'largeData': [{ min: 253, max: 277 }, { min: 303, max: 332 }, { min: 759, max: 759 }],
+  'largeData': [{ min: 253, max: 277 }, { min: 303, max: 332 }, { min: 759, max: 759 }, {min: 767, max: 767}],
   'localization': [{ min: 278, max: 302 }],
   'seval': [{ min: 333, max: 376 }],
-  'plotly_express_basic': [{ min: 377, max: 427 }],
+  'plotly_express_basic': [{ min: 377, max: 427 }, {min: 760, max: 766}],
   'plotly_express_detailed': [{ min: 428, max: 569 }],
   'plotly_express_colors': [{ min: 570, max: 749 }],
 };
