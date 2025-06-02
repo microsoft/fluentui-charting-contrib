31fe9e2ccce1433d1b9d65e898b7d3a3a8d0872c
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-06-02 08:37:44 +0530

    add localization tests (#198)
    * Move mouse in corner before collecting screenshot
    
    * Add locale specific tests
    
    * Disable update v9 job
    
    ---------
    
    Co-authored-by: Atishay Jain (from Dev Box) <atisjai@microsoft.com>

--- apps/plotly_examples/tests/constants.ts
+++
@@ -1,6 +0,0 @@
-export const totalChartExamplesCount = 767;
-
-export const chartsListWithErrors = [];
-
-export const themes = ["Light", "Dark"];
-export const modes = ["LTR", "RTL"];
---
+++ apps/plotly_examples/tests/test-matrix.ts
@@ -0,0 +1,90 @@
+const startExampleTestIndex = 0;
+export const totalChartExamplesCount = 787;
+const startExampleTestIndexLocalization = 377;
+const endExampleTestIndexLocalization = 426;
+
+export const chartsListWithErrors = [];
+
+export const themes = ["Light", "Dark"];
+export const modes = ["LTR", "RTL"];
+
+interface TestConfig {
+    theme: string;
+    mode: string;
+    startExampleIndex: number;
+    endExampleIndex: number;
+    locale: string| undefined;
+}
+
+export const testMatrix: TestConfig[] = [
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndex,
+        endExampleIndex: totalChartExamplesCount - 1,
+        locale: undefined
+    },
+    {
+        theme: "Light",
+        mode: "RTL",
+        startExampleIndex: startExampleTestIndex,
+        endExampleIndex: totalChartExamplesCount - 1,
+        locale: undefined
+    },
+    {
+        theme: "Dark",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndex,
+        endExampleIndex: totalChartExamplesCount - 1,
+        locale: undefined
+    },
+    {
+        theme: "Dark",
+        mode: "RTL",
+        startExampleIndex: startExampleTestIndex,
+        endExampleIndex: totalChartExamplesCount - 1,
+        locale: undefined
+    },
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'fr-FR'
+    },
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'de-DE'
+    },
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'ja-JP'
+    },
+    {
+        theme: "Dark",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'en-GB'
+    },
+    {
+        theme: "Light",
+        mode: "RTL",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'ar-SA'
+    },
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: 'hi-IN'
+    },
+];
