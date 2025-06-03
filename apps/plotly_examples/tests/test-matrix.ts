ab881b8535ca9bacb64ca22ed81d74f16670daa1
srmukher <120183316+srmukher@users.noreply.github.com>
2025-06-03 11:09:45 +0000

    Updating total count in test-matrix

--- apps/plotly_examples/tests/test-matrix.ts
+++ apps/plotly_examples/tests/test-matrix.ts
@@ -1,5 +1,5 @@
 const startExampleTestIndex = 0;
-export const totalChartExamplesCount = 787;
+export const totalChartExamplesCount = 839;
 const startExampleTestIndexLocalization = 377;
 const endExampleTestIndexLocalization = 569;
 
