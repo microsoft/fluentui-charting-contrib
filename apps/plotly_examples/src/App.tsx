59fa7bc5c3562b818a3920ad71d1b132bce74a2d
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-06-04 07:58:25 +0530

    fix v9 package update (#209)
    * Automate v9 package update and commit official playwright report
    
    * Enable schedule
    
    ---------
    
    Co-authored-by: Atishay Jain (from Dev Box) <atisjai@microsoft.com>

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -77,6 +77,7 @@ const App: React.FC = () => {
             label={labelWidthSwitch}
           />
           &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.23.97</Subtitle2>
+          &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20250603-0406.1</Subtitle2>
           <br />
           {isWidthSet && (<>
           <Subtitle2>Chart Width:</Subtitle2>&nbsp;&nbsp;
