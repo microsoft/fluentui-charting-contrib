7049a74ea8d190c971ee77b60119cf999b915638
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-06-05 06:57:10 +0530

    Optimize test runner

--- apps/plotly_examples/tests/DeclarativeChart_v9.spec.ts
+++ apps/plotly_examples/tests/DeclarativeChart_v9.spec.ts
@@ -1,51 +1,56 @@
-import { test, expect, Locator } from '@playwright/test';
-import * as dotenv from 'dotenv';
-import { totalChartExamplesCount, chartsListWithErrors, testMatrix } from './test-matrix';
-
-test.beforeEach(async ({ page }) => {
-  //Pass base URL as part of playwright command 
-  //ex:  npx cross-env BASE_URL='https://fluentchartstest-stage.azurewebsites.net/' npx playwright test
-  //await page.goto('http://localhost:3000/');
-});
+/* eslint-disable no-loop-func */
+import { test, expect } from '@playwright/test';
+import { chartsListWithErrors, testMatrix } from './test-matrix';
 
 for (const testConfig of testMatrix) {
   const theme = testConfig.theme;
   const mode = testConfig.mode;
   const locale = testConfig.locale;
-  //test.describe(`Declarative chart examples in ${theme} mode and ${mode} layout`, () => {
-  for (let index = testConfig.startExampleIndex; index <= testConfig.endExampleIndex; index++) {
-    const testLocaleName = locale ? `-${locale}` : '';
-    test(`Declarative chart example ${index + 1}-${theme}-${mode} mode${testLocaleName}` , async ({ browser }) => {
-      const context = await browser.newContext({ locale: locale });
-      const page = await context.newPage();
+  const testLocaleName = locale ? `-${locale}` : '';
+  test.describe('', () => {
+    let context;
+    let page;
+
+    test.beforeAll(async ({ browser }) => {
+      context = await browser.newContext({ locale });
+      page = await context.newPage();
       await page.goto(process.env.BASE_URL!);
-      const iframe = page.locator('#webpack-dev-server-client-overlay');
-      if (await iframe.count() > 0) {
-        await iframe.evaluate((el) => el.remove()).catch(() => {
-          console.warn("Failed to remove overlay iframe.");
-        });
-      }
-      await page.getByRole('combobox').first().click();
-      const listbox = page.getByRole('listbox');
-      await listbox.getByRole('option').locator(`text=${theme}`).click();
-      const rtlSwitch = page.getByTestId('rtl_switch');
-      const isCurrentlyRTL = await rtlSwitch.isChecked();
-      if ((mode === 'RTL' && !isCurrentlyRTL) || (mode === 'LTR' && isCurrentlyRTL)) {
-        await rtlSwitch.click();
-      }
-      const combobox = page.getByRole('combobox');
-      await combobox.nth(1).click();
-      const listitems = listbox.last().getByRole('option');
-      if (!chartsListWithErrors.includes(index)) {
-        await listitems.nth(index).scrollIntoViewIfNeeded();
-        await listitems.nth(index).click();
-        const chart = page.getByTestId('chart-container-v9');
-        await page.mouse.move(0, 0); // Move mouse to top-left corner
-        await expect(chart).toHaveScreenshot();
-        await combobox.last().click();
-      } else {
-        test.fail();
-      }
     });
-  };
+
+    test.afterAll(async () => {
+      await context?.close();
+    });
+    //test.describe(`Declarative chart examples in ${theme} mode and ${mode} layout`, () => {
+    for (let index = testConfig.startExampleIndex; index <= testConfig.endExampleIndex; index++) {
+      test(`Declarative chart example ${index + 1}-${theme}-${mode} mode${testLocaleName}` , async () => {
+        const iframe = page.locator('#webpack-dev-server-client-overlay');
+        if (await iframe.count() > 0) {
+          await iframe.evaluate((el) => el.remove()).catch(() => {
+            console.warn("Failed to remove overlay iframe.");
+          });
+        }
+        await page.getByRole('combobox').first().click();
+        const listbox = page.getByRole('listbox');
+        await listbox.getByRole('option').locator(`text=${theme}`).click();
+        const rtlSwitch = page.getByTestId('rtl_switch');
+        const isCurrentlyRTL = await rtlSwitch.isChecked();
+        if ((mode === 'RTL' && !isCurrentlyRTL) || (mode === 'LTR' && isCurrentlyRTL)) {
+          await rtlSwitch.click();
+        }
+        const combobox = page.getByRole('combobox');
+        await combobox.nth(1).click();
+        const listitems = listbox.last().getByRole('option');
+        if (!chartsListWithErrors.includes(index)) {
+          await listitems.nth(index).scrollIntoViewIfNeeded();
+          await listitems.nth(index).click();
+          const chart = page.getByTestId('chart-container-v9');
+          await page.mouse.move(0, 0); // Move mouse to top-left corner
+          await expect(chart).toHaveScreenshot();
+          await combobox.last().click();
+        } else {
+          test.fail();
+        }
+      });
+    };
+  });
 }
