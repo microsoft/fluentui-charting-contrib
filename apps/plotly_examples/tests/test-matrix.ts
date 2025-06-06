5a41ee436d50770c814fe0c8e85c76ec78b77979
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-06-06 14:38:17 +0530

    Debug repo structure

--- apps/plotly_examples/tests/test-matrix.ts
+++ apps/plotly_examples/tests/test-matrix.ts
@@ -1,7 +1,7 @@
 const startExampleTestIndex = 0;
-export const totalChartExamplesCount = 842;
+export const totalChartExamplesCount = 10;
 const startExampleTestIndexLocalization = 377;
-const endExampleTestIndexLocalization = 569;
+const endExampleTestIndexLocalization = 380;
 
 export const chartsListWithErrors = [];
 
