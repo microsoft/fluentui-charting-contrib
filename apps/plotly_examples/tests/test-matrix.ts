78b21028ba41f98c93b76c27ea7f4fa87efb7a19
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-06-09 13:10:24 +0530

    Enable high contrast cases

--- apps/plotly_examples/tests/test-matrix.ts
+++ apps/plotly_examples/tests/test-matrix.ts
@@ -14,6 +14,7 @@ interface TestConfig {
     startExampleIndex: number;
     endExampleIndex: number;
     locale: string| undefined;
+    highContrast: boolean;
 }
 
 export const testMatrix: TestConfig[] = [
@@ -22,69 +23,95 @@ export const testMatrix: TestConfig[] = [
         mode: "LTR",
         startExampleIndex: startExampleTestIndex,
         endExampleIndex: totalChartExamplesCount - 1,
-        locale: undefined
+        locale: undefined,
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "RTL",
         startExampleIndex: startExampleTestIndex,
         endExampleIndex: totalChartExamplesCount - 1,
-        locale: undefined
+        locale: undefined,
+        highContrast: false
     },
     {
         theme: "Dark",
         mode: "LTR",
         startExampleIndex: startExampleTestIndex,
         endExampleIndex: totalChartExamplesCount - 1,
-        locale: undefined
+        locale: undefined,
+        highContrast: false
     },
     {
         theme: "Dark",
         mode: "RTL",
         startExampleIndex: startExampleTestIndex,
         endExampleIndex: totalChartExamplesCount - 1,
-        locale: undefined
+        locale: undefined,
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "LTR",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'fr-FR'
+        locale: 'fr-FR',
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "LTR",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'de-DE'
+        locale: 'de-DE',
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "LTR",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'ja-JP'
+        locale: 'ja-JP',
+        highContrast: false
     },
     {
         theme: "Dark",
         mode: "LTR",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'en-GB'
+        locale: 'en-GB',
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "RTL",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'ar-SA'
+        locale: 'ar-SA',
+        highContrast: false
     },
     {
         theme: "Light",
         mode: "LTR",
         startExampleIndex: startExampleTestIndexLocalization,
         endExampleIndex: endExampleTestIndexLocalization,
-        locale: 'hi-IN'
+        locale: 'hi-IN',
+        highContrast: false
+    },
+    {
+        theme: "Light",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: undefined,
+        highContrast: true
+    },
+    {
+        theme: "Dark",
+        mode: "LTR",
+        startExampleIndex: startExampleTestIndexLocalization,
+        endExampleIndex: endExampleTestIndexLocalization,
+        locale: undefined,
+        highContrast: true
     },
 ];
