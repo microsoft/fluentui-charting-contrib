0cba0f8cea048e09b11977f35ae323da89a00240
AtishayMsft <98592573+AtishayMsft@users.noreply.github.com>
2025-07-07 05:43:13 +0000

    (chore) (auto) update @fluentui/react-charts to version 0.0.0-nightly-20250707-0406.1

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -77,7 +77,7 @@ const App: React.FC = () => {
             label={labelWidthSwitch}
           />
           &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.24.11</Subtitle2>
-          &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20250703-0405.1</Subtitle2>
+          &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20250707-0406.1</Subtitle2>
           <br />
           {isWidthSet && (<>
           <Subtitle2>Chart Width:</Subtitle2>&nbsp;&nbsp;
