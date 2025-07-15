6f02fb2254815e6379dc2aee213ab392d3a2f84b
srmukher <120183316+srmukher@users.noreply.github.com>
2025-07-15 14:56:38 +0000

    Adding heatmap examples

--- apps/plotly_examples/tests/test-matrix.ts
+++ apps/plotly_examples/tests/test-matrix.ts
@@ -1,5 +1,5 @@
 const startExampleTestIndex = 0;
-export const totalChartExamplesCount = 892;
+export const totalChartExamplesCount = 895;
 const startExampleTestIndexLocalization = 377;
 const endExampleTestIndexLocalization = 569;
 
