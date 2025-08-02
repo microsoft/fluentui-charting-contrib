5b7e4d1d2bcecb6d132474f3c875abbcb53dbc38
Kumar Kshitij <kumarkshitij@microsoft.com>
2025-08-03 00:12:46 +0530

    add log axis examples

--- apps/plotly_examples/src/components/DeclarativeChart.tsx
+++ apps/plotly_examples/src/components/DeclarativeChart.tsx
@@ -59,7 +59,7 @@ type DataType =
   | 'advanced_scenarios';
 
 const dataTypeRanges = {
-  'general': [{ min: 1, max: 252 }, {min: 750, max: 758 }, {min: 840, max: 846 }, {min: 848, max: 853}, {min: 855, max: 856}, {min: 871, max: 871}, {min: 893, max: 912}],
+  'general': [{ min: 1, max: 252 }, {min: 750, max: 758 }, {min: 840, max: 846 }, {min: 848, max: 853}, {min: 855, max: 856}, {min: 871, max: 871}, {min: 893, max: 922}],
   'largeData': [{ min: 253, max: 277 }, { min: 303, max: 332 }, { min: 759, max: 759 }, {min: 767, max: 767}],
   'localization': [{ min: 278, max: 302 }],
   'seval': [{ min: 333, max: 376 }],
