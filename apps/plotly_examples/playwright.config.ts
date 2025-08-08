2a1abcb3aee26ffbc8ce171aa4547f18d799e8bd
Atishay Jain (from Dev Box) <atisjai@microsoft.com>
2025-08-08 16:53:15 +0530

    Minor fix

--- apps/plotly_examples/playwright.config.ts
+++ apps/plotly_examples/playwright.config.ts
@@ -30,8 +30,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     timezoneId: 'GMT',
+    trace: 'off',
 /*     trace: 'on',
     video: {
       mode: 'on', // or 'retain-on-failure', 'on-first-retry'
