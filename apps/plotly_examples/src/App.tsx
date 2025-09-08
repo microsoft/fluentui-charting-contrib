416ff13229371fa931deea02c8ecc85582587f05
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-09-08 13:57:38 +0530

    (chore) (auto) update @fluentui/react-charting to version 5.24.27 (#490)

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -76,7 +76,7 @@ const App: React.FC = () => {
             onChange={handleWidthSwitchChange}
             label={labelWidthSwitch}
           />
-          &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.24.26</Subtitle2>
+          &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.24.27</Subtitle2>
           &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20250903-0406.1</Subtitle2>
           <br />
           {isWidthSet && (<>
