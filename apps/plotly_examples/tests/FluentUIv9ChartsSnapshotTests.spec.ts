8ff234dca442b7ed90aaa263380e12927eb423f5
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-09-18 17:10:58 +0530

    removed all the snaps

--- apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
+++ apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
@@ -139,21 +139,21 @@ const verticalStackedBarChartStories = [
 
 const charts = [
   { name: 'AreaChart', path: 'charts-areachart--docs', selector: '#story--charts-areachart--area-chart-basic--primary-inner', stories: areaChartStories },
-  { name: 'DonutChart', path: 'charts-DonutChart--docs', selector: '#story--charts-donutchart--donut-chart-basic--primary-inner' , stories: donutChartStories},
-  { name: 'FunnelChart', path: 'charts-FunnelChart--docs', selector: '#story--charts-funnelchart--funnel-chart-basic--primary-inner', stories: funnelChartStories },
-  { name: 'GanttChart', path: 'charts-GanttChart--docs', selector: '#story--charts-ganttchart--gantt-chart-basic--primary-inner', stories: ganttChartStories },
-  { name: 'GaugeChart', path: 'charts-GaugeChart--docs', selector: '#story--charts-gaugechart--gauge-chart-basic--primary-inner' , stories: guageChartStories},
-  { name: 'GroupedVerticalBarChart', path: 'charts-GroupedVerticalBarChart--docs', selector: '#story--charts-groupedverticalbarchart--grouped-vertical-bar-default--primary-inner' , stories: gvbChartStories},
-  { name: 'HeatMapChart', path: 'charts-HeatMapChart--docs', selector: '#story--charts-heatmapchart--heat-map-chart-basic--primary-inner', stories: heatMapChartStories },
-  { name: 'HorizontalBarChart', path: 'charts-HorizontalBarChart--docs', selector: '#story--charts-horizontalbarchart--horizontal-bar-basic--primary-inner' , stories: horizontalBarChartStories},
-  { name: 'HorizontalBarChartWithAxis', path: 'charts-HorizontalBarChartWithAxis--docs', selector: '#story--charts-horizontalbarchartwithaxis--horizontal-bar-with-axis-basic--primary-inner' , stories: horizontalBarChartWithAxisStories},
-  { name: 'Legends', path: 'charts-Legends--docs', selector: '#story--charts-legends--legends-basic--primary-inner', stories: legendsStories },
-  { name: 'LineChart', path: 'charts-LineChart--docs', selector: '#story--charts-linechart--line-chart-basic--primary-inner', stories: lineChartStories },
-  { name: 'SankeyChart', path: 'charts-SankeyChart--docs', selector: '#story--charts-sankeychart--sankey-chart-basic--primary-inner', stories: sankeyChartStories },
-  { name: 'ScatterChart', path: 'charts-ScatterChart--docs', selector: '#story--charts-scatterchart--scatter-chart-default--primary-inner', stories: scatterChartStories },
-  { name: 'Sparkline', path: 'charts-Sparkline--docs', selector: '#story--charts-sparkline--sparkline-basic--primary-inner', stories: sparklineChartStories },
-  { name: 'VerticalBarChart', path: 'charts-VerticalBarChart--docs', selector: '#story--charts-verticalbarchart--vertical-bar-default--primary-inner', stories: verticalBarChartStories },
-  { name: 'VerticalStackedBarChart', path: 'charts-VerticalStackedBarChart--docs', selector: '#story--charts-verticalstackedbarchart--vertical-stacked-bar-default--primary-inner', stories: verticalStackedBarChartStories },
+  // { name: 'DonutChart', path: 'charts-DonutChart--docs', selector: '#story--charts-donutchart--donut-chart-basic--primary-inner' , stories: donutChartStories},
+  // { name: 'FunnelChart', path: 'charts-FunnelChart--docs', selector: '#story--charts-funnelchart--funnel-chart-basic--primary-inner', stories: funnelChartStories },
+  // { name: 'GanttChart', path: 'charts-GanttChart--docs', selector: '#story--charts-ganttchart--gantt-chart-basic--primary-inner', stories: ganttChartStories },
+  // { name: 'GaugeChart', path: 'charts-GaugeChart--docs', selector: '#story--charts-gaugechart--gauge-chart-basic--primary-inner' , stories: guageChartStories},
+  // { name: 'GroupedVerticalBarChart', path: 'charts-GroupedVerticalBarChart--docs', selector: '#story--charts-groupedverticalbarchart--grouped-vertical-bar-default--primary-inner' , stories: gvbChartStories},
+  // { name: 'HeatMapChart', path: 'charts-HeatMapChart--docs', selector: '#story--charts-heatmapchart--heat-map-chart-basic--primary-inner', stories: heatMapChartStories },
+  // { name: 'HorizontalBarChart', path: 'charts-HorizontalBarChart--docs', selector: '#story--charts-horizontalbarchart--horizontal-bar-basic--primary-inner' , stories: horizontalBarChartStories},
+  // { name: 'HorizontalBarChartWithAxis', path: 'charts-HorizontalBarChartWithAxis--docs', selector: '#story--charts-horizontalbarchartwithaxis--horizontal-bar-with-axis-basic--primary-inner' , stories: horizontalBarChartWithAxisStories},
+  // { name: 'Legends', path: 'charts-Legends--docs', selector: '#story--charts-legends--legends-basic--primary-inner', stories: legendsStories },
+  // { name: 'LineChart', path: 'charts-LineChart--docs', selector: '#story--charts-linechart--line-chart-basic--primary-inner', stories: lineChartStories },
+  // { name: 'SankeyChart', path: 'charts-SankeyChart--docs', selector: '#story--charts-sankeychart--sankey-chart-basic--primary-inner', stories: sankeyChartStories },
+  // { name: 'ScatterChart', path: 'charts-ScatterChart--docs', selector: '#story--charts-scatterchart--scatter-chart-default--primary-inner', stories: scatterChartStories },
+  // { name: 'Sparkline', path: 'charts-Sparkline--docs', selector: '#story--charts-sparkline--sparkline-basic--primary-inner', stories: sparklineChartStories },
+  // { name: 'VerticalBarChart', path: 'charts-VerticalBarChart--docs', selector: '#story--charts-verticalbarchart--vertical-bar-default--primary-inner', stories: verticalBarChartStories },
+  // { name: 'VerticalStackedBarChart', path: 'charts-VerticalStackedBarChart--docs', selector: '#story--charts-verticalstackedbarchart--vertical-stacked-bar-default--primary-inner', stories: verticalStackedBarChartStories },
 ];
 
 const themes = ['web-light', 'web-dark'];
@@ -165,12 +165,12 @@ async function loadChartPage(
   theme: string,
   mode: string
 ) {
-  await page.goto('/?path=/docs/');
+  await page.goto('http://localhost:3000/?path=/docs/introduction--docs');
   await page.getByLabel('Shortcuts').click();
   await page.locator('#list-item-T').click();
   await page.getByRole('button', { name: /Theme:/ }).click();
   await page.locator(`#list-item-${theme}`).click();
-  await page.getByRole('button', { name: chart.name, exact: true }).click();
+  await page.getByRole('link', { name: chart.name, exact: true }).click();
   // Check current direction and only click if needed
   const directionButton = await page.getByRole('button', { name: /Direction:/ });
   const directionText = await directionButton.textContent();
@@ -253,7 +253,7 @@ async function interactWithSwitches(frame: any, imgId: string, screenshotName: s
     const label = await frame.locator(`label[for="${await control.getAttribute('id')}"]`).textContent();
     const labelText = label.split('(')[0].trim();
     const path = `apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts-snapshots/${screenshotName}-${labelText.toLowerCase()} checkbox-click.png`;
-    await frame.locator(`#${imgId}`).screenshot({ path: path });
+   expect(frame.locator(`#${imgId}`)).toHaveScreenshot();
   }
 }
 
@@ -263,41 +263,41 @@ for (const chart of charts) {
       test.describe(`${chart.name} [${theme}] [${mode}]`, () => {
         for (const exampleSelector of chart.stories) {
           const testCaseName = exampleSelector.split('--').slice(-1)[0].trim().replace(/-inner/g, '');
-          test(`Legend Action - ${testCaseName}`, async ({ page }) => {
-            const frame = await loadChartPage(page, chart, theme, mode);
-            const example = frame.locator(exampleSelector);
-            await example.scrollIntoViewIfNeeded();
-            const imgId = await example.getAttribute('id');
-            const dataName = await example.getAttribute('data-name');
-            if (imgId) {
-              const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
-              await interactWithLegends(frame, imgId, screenshotName);
-            }
-          });
+          // test(`Legend Action - ${testCaseName}`, async ({ page }) => {
+          //   const frame = await loadChartPage(page, chart, theme, mode);
+          //   const example = frame.locator(exampleSelector);
+          //   await example.scrollIntoViewIfNeeded();
+          //   const imgId = await example.getAttribute('id');
+          //   const dataName = await example.getAttribute('data-name');
+          //   if (imgId) {
+          //     const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
+          //     await interactWithLegends(frame, imgId, screenshotName);
+          //   }
+          // });
 
-          test(`Radio button Action - ${testCaseName}`, async ({ page }) => {
-            const frame = await loadChartPage(page, chart, theme, mode);
-            const example = frame.locator(exampleSelector);
-            await example.scrollIntoViewIfNeeded();
-            const imgId = await example.getAttribute('id');
-            const dataName = await example.getAttribute('data-name');
-            if (imgId) {
-              const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
-              await interactWithRadios(frame, imgId, screenshotName);
-            }
-          });
+          // test(`Radio button Action - ${testCaseName}`, async ({ page }) => {
+          //   const frame = await loadChartPage(page, chart, theme, mode);
+          //   const example = frame.locator(exampleSelector);
+          //   await example.scrollIntoViewIfNeeded();
+          //   const imgId = await example.getAttribute('id');
+          //   const dataName = await example.getAttribute('data-name');
+          //   if (imgId) {
+          //     const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
+          //     await interactWithRadios(frame, imgId, screenshotName);
+          //   }
+          // });
 
-          test(`slider Action - ${testCaseName}`, async ({ page }) => {
-            const frame = await loadChartPage(page, chart, theme, mode);
-            const example = frame.locator(exampleSelector);
-            await example.scrollIntoViewIfNeeded();
-            const imgId = await example.getAttribute('id');
-            const dataName = await example.getAttribute('data-name');
-            if (imgId) {
-              const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
-              await interactWithSliders(frame, imgId, screenshotName);
-            }
-          });
+          // test(`slider Action - ${testCaseName}`, async ({ page }) => {
+          //   const frame = await loadChartPage(page, chart, theme, mode);
+          //   const example = frame.locator(exampleSelector);
+          //   await example.scrollIntoViewIfNeeded();
+          //   const imgId = await example.getAttribute('id');
+          //   const dataName = await example.getAttribute('data-name');
+          //   if (imgId) {
+          //     const screenshotName = sanitizeFileName(`${dataName || `${chart.name} basic`} [${theme}] [${mode}]`);
+          //     await interactWithSliders(frame, imgId, screenshotName);
+          //   }
+          // });
 
            test(`Switch Action - ${testCaseName}`, async ({ page }) => {
             const frame = await loadChartPage(page, chart, theme, mode);
