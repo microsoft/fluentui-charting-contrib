613c73d78de6137889dadbecec7900b0f87bcaa3
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-10-12 20:19:58 +0530

    Revert "Use react 19 (#541)"
    This reverts commit 1f6a832468bf6fcda7b437cc58d88b3a7c99f461.

--- apps/plotly_examples/src/index.tsx
+++ apps/plotly_examples/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = createRoot(document.getElementById('root')!);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
