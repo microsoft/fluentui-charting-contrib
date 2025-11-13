4d09a57c89aba2d2110166186f8068fdb0e1a324
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-11-13 16:23:19 +0530

    added height slider

--- apps/plotly_examples/src/components/PlotlyChart.tsx
+++ apps/plotly_examples/src/components/PlotlyChart.tsx
@@ -1,8 +1,30 @@
 import Plotly from "plotly.js";
 import createPlotlyComponent from "react-plotly.js/factory";
 const Plot = createPlotlyComponent(Plotly);
+interface PlotlyChartProps {
+  schema: any;
+  width?: number;
+  height?: number;
+}
 
-export default function PlotlyChart(schema: any) {
-  const { data, layout } = schema.schema;
-  return <Plot data={data} layout={layout} />;
+export default function PlotlyChart({ schema, width, height }: PlotlyChartProps) {
+  const { data, layout } = schema;
+
+  return (
+    <div
+      key={`plotly-${width || 'auto'}-${height || 'auto'}`}
+      style={{
+        height: height ? `${height}px` : '500px',
+        width: width ? `${width}px` : '100%',
+        overflow: 'visible'
+      }}
+    >
+      <Plot 
+        data={data} 
+        layout={layout}
+        style={{ width: '100%', height: '100%' }}
+        config={{ responsive: true }}
+      />
+    </div>
+  );
 }
