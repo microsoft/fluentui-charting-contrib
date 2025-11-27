ab962dc9ceebb5dd55d3a24facd70f48fc987211
AtishayMsft <98592573+AtishayMsft@users.noreply.github.com>
2025-11-27 07:59:26 +0000

    (chore) (auto) update @fluentui/react-charting to version 5.25.3

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -95,7 +95,7 @@ const App: React.FC = () => {
             onChange={handleChartOrderSwitchChange}
             label={labelChartOrderSwitch}
           />
-          &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.25.2</Subtitle2>
+          &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.25.3</Subtitle2>
           &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20251124-0406.1</Subtitle2>
           <br />
           {isDimensionSlidersEnabled && (<>
