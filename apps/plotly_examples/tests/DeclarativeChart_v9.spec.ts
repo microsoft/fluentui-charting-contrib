5baea438b031f736942f77cb46f856d9762089f8
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-12-04 14:37:06 +0530

    added v9 charts downlod button snaps tests and workflow

--- apps/plotly_examples/tests/DeclarativeChart_v9.spec.ts
+++ apps/plotly_examples/tests/DeclarativeChart_v9.spec.ts
@@ -1,63 +1,63 @@
+/* eslint-disable no-loop-func */
 import { test, expect } from '@playwright/test';
-import { startExampleTestIndexLocalization, endExampleTestIndexLocalization, chartsListWithErrors } from './test-matrix';
+import { chartsListWithErrorsV9, testMatrix } from './test-matrix';
 
-test.beforeEach(async ({ page }) => {
-  //Pass base URL as part of playwright command 
-  // ex:  npx cross-env BASE_URL='https://fluentchartstest-stage.azurewebsites.net/' npx playwright test
-    await page.goto(process.env.BASE_URL!);
-});
+for (const testConfig of testMatrix) {
+  const theme = testConfig.theme;
+  const mode = testConfig.mode;
+  const locale = testConfig.locale;
+  const testLocaleName = locale ? `-${locale}` : '';
+  const highContrast = testConfig.highContrast ? '-HighContrast' : '';
+  test.describe('', () => {
+    let context;
+    let page;
 
-for (let index = startExampleTestIndexLocalization; index < endExampleTestIndexLocalization; index++) {
-  test(`Declarative chart example ${index + 1}`, async ({ page }) => {
-    const iframe = page.locator('#webpack-dev-server-client-overlay');
-    if (await iframe.count() > 0) {
-      await iframe.evaluate((el) => el.remove()).catch(() => {
-        console.warn("Failed to remove overlay iframe.");
+    test.beforeAll(async ({ browser }) => {
+      if (testConfig.highContrast) {
+        context = await browser.newContext({ locale, forcedColors: 'active', colorScheme: theme === 'Dark'? 'dark': 'light' });
+      }
+      else {
+        context = await browser.newContext({ locale });
+      }
+
+      page = await context.newPage();
+      await page.goto(process.env.BASE_URL!);
+    });
+
+    test.afterAll(async () => {
+      await context?.close();
+    });
+    //test.describe(`Declarative chart examples in ${theme} mode and ${mode} layout`, () => {
+    for (let index = testConfig.startExampleIndex; index <= testConfig.endExampleIndex; index++) {
+      test(`Declarative chart example ${index + 1}-${theme}-${mode} mode${testLocaleName}${highContrast}` , async () => {
+        const iframe = page.locator('#webpack-dev-server-client-overlay');
+        if (await iframe.count() > 0) {
+          await iframe.evaluate((el) => el.remove()).catch(() => {
+            console.warn("Failed to remove overlay iframe.");
+          });
+        }
+        await page.getByRole('combobox').first().click();
+        const listbox = page.getByRole('listbox');
+        await listbox.getByRole('option').locator(`text=${theme}`).click();
+        const rtlSwitch = page.getByTestId('rtl_switch');
+        const isCurrentlyRTL = await rtlSwitch.isChecked();
+        if ((mode === 'RTL' && !isCurrentlyRTL) || (mode === 'LTR' && isCurrentlyRTL)) {
+          await rtlSwitch.click();
+        }
+        const combobox = page.getByRole('combobox');
+        await combobox.nth(1).click();
+        const listitems = listbox.last().getByRole('option');
+        await listitems.nth(index).scrollIntoViewIfNeeded();
+        await listitems.nth(index).click();
+        const chart = page.getByTestId('chart-container-v9');
+        await page.mouse.move(0, 0); // Move mouse to top-left corner
+        if (!chartsListWithErrorsV9.includes(index + 1)) {
+          await expect(chart).toHaveScreenshot();
+          await combobox.last().click();
+        } else {
+          await expect(chart).not.toHaveScreenshot();
+        }
       });
-    }
-    const combobox = page.getByRole('combobox');
-    await combobox.nth(1).click();
-    const listbox = page.getByRole('listbox');
-    const listitems = listbox.last().getByRole('option');
-    if (!chartsListWithErrors.includes(index)) {
-      await listitems.nth(index).scrollIntoViewIfNeeded();
-      await listitems.nth(index).click();
-      const chart = page.getByTestId('chart-container');
-      await expect(chart).toHaveScreenshot();
-    } else {
-      test.fail();
-    }
+    };
   });
-
-  test(`Declarative chart example ${index + 1} Download V9 Chart Image`, async ({ page }) => {
-    const iframe = page.locator('#webpack-dev-server-client-overlay');
-    if (await iframe.count() > 0) {
-      await iframe.evaluate((el) => el.remove()).catch(() => {
-        console.warn("Failed to remove overlay iframe.");
-      });
-    } 
-
-    const combobox = page.getByRole('combobox');
-    await combobox.nth(1).click();
-    const listbox = page.getByRole('listbox');
-    const listitems = listbox.last().getByRole('option');
-    await listitems.nth(index).scrollIntoViewIfNeeded();
-    await listitems.nth(index).click();
-    const [download] = await Promise.all([
-      page.waitForEvent('download'),
-      page.getByRole('button', { name: "Download V9 Chart as Image" }).click()
-    ]);
-    const downloadedImageBuffer = await stream2buffer(await download.createReadStream())
-    expect(downloadedImageBuffer).toMatchSnapshot(`downloaded-declarative-chart-example-${index + 1}.png`)
-    await download.delete()
-  });
-};
-
-async function stream2buffer(stream: Stream) {
-  return new Promise<Buffer>((resolve, reject) => {
-    const _buf = Array<any>();
-    stream.on("data", chunk => _buf.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(_buf)));
-    stream.on("error", err => reject(`error converting stream - ${err}`));
-  });
-} 
+}
