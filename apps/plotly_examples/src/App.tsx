d147571203fb7dd053cc46bf8bd524ffa274bf00
AtishayMsft <98592573+AtishayMsft@users.noreply.github.com>
2025-12-05 05:44:31 +0000

    (chore) (auto) update @fluentui/react-charts to version 0.0.0-nightly-20251205-0406.1

--- apps/plotly_examples/src/App.tsx
+++ apps/plotly_examples/src/App.tsx
@@ -97,7 +97,7 @@ const App: React.FC = () => {
           />
 
           &nbsp;&nbsp;<Body2>@fluentui/react-charting &nbsp;</Body2><Subtitle2>v5.25.3</Subtitle2>
-          &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20251204-0407.1</Subtitle2>
+          &nbsp;&nbsp;<Body2>@fluentui/react-charts &nbsp;</Body2><Subtitle2>0.0.0-nightly-20251205-0406.1</Subtitle2>
           <br />
           {isDimensionSlidersEnabled && (<>
           <div style={{ display: 'flex', flexDirection: isRTL ? 'row-reverse' : 'row', alignItems: 'center', gap: '20px' }}>
