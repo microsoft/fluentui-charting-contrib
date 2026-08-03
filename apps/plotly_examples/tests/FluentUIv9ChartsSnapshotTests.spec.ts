52faae54539f13f5242d583a6b176e480684cf58
v-baambati <132879294+v-baambati@users.noreply.github.com>
2026-08-03 11:39:32 +0000

    commented all other tests

--- apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
+++ apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
@@ -171,7 +171,7 @@ async function loadChartPage(
   await page.waitForTimeout(2000);
 
   await page.locator('iframe[title="storybook-ref-charts"]').contentFrame().getByRole('button', { name: 'Theme' }).press('Enter');
-  await page.locator('iframe[title="storybook-ref-charts"]').contentFrame().getByText(theme).click();
+  await page.locator('iframe[title="storybook-ref-charts"]').contentFrame().getByText(theme).press('Enter');
 
   // Only check RTL direction if mode is RTL
   if (mode === 'RTL') {
