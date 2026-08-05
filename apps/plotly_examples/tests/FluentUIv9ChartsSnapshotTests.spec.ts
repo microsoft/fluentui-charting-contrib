e3382de050564f0f2bebe31af63ea93b90567b99
v-baambati <132879294+v-baambati@users.noreply.github.com>
2026-08-05 05:09:55 +0000

    disabled tests

--- apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
+++ apps/plotly_examples/tests/FluentUIv9ChartsSnapshotTests.spec.ts
@@ -157,7 +157,7 @@ const charts = [
 ];
 
 const themes = ['Web Light', 'Web Dark'];
-const modes = ['RTL'];
+const modes = ['LTR', 'RTL'];
 
 async function loadChartPage(
   page: any,
@@ -176,7 +176,7 @@ async function loadChartPage(
   // Only check RTL direction if mode is RTL
   if (mode === 'RTL') {
       const dirSwitch = storybookFrame.getByRole('menuitemradio', { name: 'RTL' });
-      await dirSwitch.check({ force: true });
+      await dirSwitch.click();
   }
   const chartContainer = page.locator('iframe[title="storybook-ref-charts"]');
   const frame = await chartContainer.contentFrame();
